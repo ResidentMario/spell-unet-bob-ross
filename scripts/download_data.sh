86a450e6f988a30a505d060e9ee68053656a619c
Aleksey Bilogur <aleksey@residentmar.io>
2020-11-18 12:59:12 -0500

    +

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -3,3 +3,4 @@ set -v
 kaggle datasets download residentmario/segmented-bob-ross-images
 unzip segmented-bob-ross-images.zip -d /mnt/segmented-bob-ross-images/
 rm segmented-bob-ross-images.zip
+echo "Done downloading data."
